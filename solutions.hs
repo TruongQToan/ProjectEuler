16f20bd9b6422f5e71a506c2c52567b6b832cd1a
toantruong95 <toantruong@trustingsocial.com>
2020-03-14 21:45:40 +0700

    Add p4

--- solutions.hs
+++ solutions.hs
@@ -31,4 +31,23 @@ prime_factors n =
 euler3 :: Integer->Integer
 euler3 n = last $ prime_factors n
 
+--- Problem 4
+palindrome:: Integer → Bool
+palindrome x = reversal x == x
 
+reversal :: Integer → Integer
+reversal = go 0
+    where go a 0 = a
+          go a b = let (q, r) = b `quotRem` 10 in go (a*10+r) q
+
+palindrome_product :: Integer → Integer → Integer → Integer → Integer
+palindrome_product lo_a hi_a lo_b hi_b = go hi_a hi_b (lo_a*lo_b)
+    where go a b max = if a == lo_a-1
+                        then max
+                        else if b == lo_b-1
+                            then go (a-1) hi_b max
+                            else let max' = if palindrome (a*b) && a*b > max then a*b else max
+                                 in go a (b-1) max'
+
+euler4 :: Integer
+euler4 = palindrome_product 100 999 100 999
